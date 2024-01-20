9cba16afb7dab06c3031236577c105522798937d
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-01-19 16:05:27 -0800

    Auto Property
    Auto Property additions

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -4,7 +4,14 @@ public class LogFactory
 {
     public BaseLogger CreateLogger(string className)
     {
+        LogFactory logger = new LogFactory();
+        logger.ClassName = className;
 
         return null;
+    }
+
+    public void ConfigureFileLogger(string filePath)
+    {
+        
     }
 }
