65e10c4cc05397217e5de33f5b8dee884d7fcb68
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-01-24 15:45:39 -0800

    logger test

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,29 +10,31 @@ namespace Logger.Tests;
 [TestClass]
 public class FileLoggerTests
 {
-    //[TestMethod]
-    //public void Log_WithPath_AppendsToOwnLine()
-    //{
-    //    string fileName = "file.txt";
-    //    // Arrange
-    //    string virtualPath = Path.Combine("C:\\Users\\Cynthia\\Desktop", fileName);
-    //    FileLogger fileLogger = new (virtualPath);
+    [TestMethod]
+    public void Log_WithPath_AppendsToOwnLine()
+    {
+        string fileName = "file.txt";
+        // Arrange
+        string virtualPath = Path.Combine("C:\\Users\\Cynthia\\Desktop", fileName);
+        FileLogger fileLogger = new (virtualPath);
 
-    //    // Act
-    //    fileLogger.Log(LogLevel.Warning, "Warnings");
+        // Act
+        fileLogger.Log(LogLevel.Warning, "Warnings");
         
-    //    string log = $"{System.DateTime.Now} {"FileLogger"} {LogLevel.Warning}: {"Warnings"}";
+        string log = $"{System.DateTime.Now} {"FileLogger"} {LogLevel.Warning}: {"Warnings"}";
         
-    //    string read="";
-        
-    //    if (File.Exists(virtualPath))
-    //    {
-    //        // Create a file to write to.
-           
-    //        StreamReader sr = File.OpenText(virtualPath);
-    //        read = sr.ReadToEnd();
-    //    }
-    //    // Assert
-    //    Assert.AreEqual(log, read);
-    //}
+        string read="";
+        string contents = File.ReadLines(virtualPath).Last();
+
+        //if (File.Exists(virtualPath))
+        //{
+        //    // Create a file to write to.
+        //    StreamReader sr = File.OpenText(virtualPath);;
+        //    read = sr.ReadToEnd();
+        //    read.Trim();
+        //}
+
+        // Assert
+        Assert.AreEqual(log, contents);
+    }
 }
