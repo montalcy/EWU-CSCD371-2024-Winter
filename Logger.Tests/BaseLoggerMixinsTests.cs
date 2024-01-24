d7f6cfa659247d8a622d620a74db6bf56de392c3
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-01-24 15:22:19 -0800

    another fix :(

--- Logger.Tests/BaseLoggerMixinsTests.cs
+++ Logger.Tests/BaseLoggerMixinsTests.cs
@@ -51,7 +51,7 @@ public class BaseLoggerMixinsTests
         // Arrange
 
         // Act
-        BaseLoggerMixins.Debug(null, "");
+        BaseLoggerMixins.Debug(null!, "");
 
         // Assert
     }
