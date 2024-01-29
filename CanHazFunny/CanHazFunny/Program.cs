8c413ce8d6af6c4f69065ec616accdb5c7f36c8a
Hadi <amallah@ewu.edu>
2024-01-29 10:42:35 -0800

    FIx

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        new Jester(new IJokeOutput(), new IJokeService()).TellJoke();
+        new Jester(new JokeOutput(), new JokeService()).TellJoke();
         //Feel free to use your own setup here - this is just provided as an example
         //new Jester( new SomeReallyCoolOutputClass(), new SomeJokeServiceClass() ).TellJoke();
     }
