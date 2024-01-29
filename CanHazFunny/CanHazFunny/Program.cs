ec902149c2f652b10f0f1252bc10f8993baffa28
Hadi <amallah@ewu.edu>
2024-01-29 08:42:48 -0800

    Jester - clean code, Program - FullSetup

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -4,7 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        new Jester(new IJokeOutput(), new IJokeService()).TellJoke();
         //Feel free to use your own setup here - this is just provided as an example
-        //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
+        //new Jester( new SomeReallyCoolOutputClass(), new SomeJokeServiceClass() ).TellJoke();
     }
 }
