789e2e1c22ef473dcb06a43e6485259f4684bedb
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-01-29 21:09:46 -0800

    again some fixes based on pull

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -3,7 +3,7 @@ namespace CanHazFunny;
 
 public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
         JokeService jokeService = new();
         OutputtingJoke outputtingJoke = new();
