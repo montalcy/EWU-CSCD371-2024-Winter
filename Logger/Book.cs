bb26dd3f497a6c339b69464d12d5a6d7642bbee4
Hadi <amallah@ewu.edu>
2024-02-04 18:03:31 -0800

    FullName record Class setup

--- Logger/Book.cs
+++ Logger/Book.cs
@@ -15,8 +15,6 @@ namespace Logger
             Name = name;
         }
 
-        
-
         private string GetBookName()
         {
             //throw new NotImplementedException();
---
+++ Logger/FullName.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public record class FullName
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string? MiddleName { get; set; } = string.Empty;
+
+        public FullName(string firstName, string lastName) 
+        { 
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public FullName(string firstName, string lastName, string? middleName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MiddleName = middleName;
+        }
+
+        
+    }
+}
