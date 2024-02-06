008b33a6fd1c326af7f26594a4384684465f15c9
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-02-05 16:57:01 -0800

    More tests

--- Logger.Tests/StorageTests.cs
+++ Logger.Tests/StorageTests.cs
@@ -11,6 +11,7 @@ public class StorageTests
     [Fact]
     public void StudentConstructor_CheckingEquality_Equal()
     {
+        //should return true since they are the same type of records
         FullName full = new("First", "Last");
         FullName full2 = new("First2", "Last2");
         Student student1 = new("First", "Last", full, null);
@@ -22,6 +23,7 @@ public class StorageTests
     [Fact]
     public void EmployeeConstructor_CheckingEquality_Equal()
     {
+        //should return true since they are the same type of records
         FullName full = new("First", "Last");
         FullName full2 = new("First2", "Last2");
         Employee et1 = new("First", "Last", full, null);
@@ -30,5 +32,24 @@ public class StorageTests
 
     }
 
+    [Fact]
+    public void BookConstructor_CheckingEquality_Equal()
+    {
+        //should return true since they are the same type of records
+        Book book1 = new("Book");
+        Book book2 = new("Book");
+        Assert.True(book1.Equals(book2));
+    }
+    
+    [Fact]
+    public void Constructor_CheckingEqualityWithDifferentRecords_NotEqual()
+    {
+        //should return false since they are different records
+        FullName full = new("First", "Last");
+        FullName full2 = new("First2", "Last2");
+        Employee et1 = new("First", "Last", full, null);
+        Book book2 = new("Book");
+        Assert.False(et1.Equals(book2));
+    }
 }
 
