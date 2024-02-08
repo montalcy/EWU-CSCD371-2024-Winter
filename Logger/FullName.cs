e09348f25f9c40d4be2900bd44a3a880d9a0deca
Hadi <amallah@ewu.edu>
2024-02-08 11:16:53 -0800

    Added required comments on FullName
    Wrote about why we used Immutable(read only) and comment of why we used struct for it(value type).

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -5,11 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Logger;
-public record class FullName
+public readonly record struct FullName
 {
-    private string FirstName { get; set; }
-    private string LastName { get; set; } 
-    private string? MiddleName { get; set; } 
+    private string FirstName { get; }
+    private string LastName { get; } 
+    private string? MiddleName { get; } 
 
     public FullName(string firstName, string lastName)
     {
@@ -28,4 +28,5 @@ public record class FullName
        MiddleName == null ? FirstName + " " + MiddleName + " " + LastName : FirstName + " " + LastName;
 
 }
-
+// Decided on it to be struct after being reminded that it would be better as a value type as all its doing is dealing with a few variables
+// chose for it to be immutable as having it be mutable could open it up for exploits, used readonly modifier
