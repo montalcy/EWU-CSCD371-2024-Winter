d2aa2fed8494336b7a376f06a4ad597d98883296
Hadi <amallah@ewu.edu>
2024-02-11 20:11:06 -0800

    Defined next method and toString method in Node class

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -1,8 +1,21 @@
 ﻿namespace GenericsHomework
 {
-    public class Node
+    public class Node<T>
     {
         private T _value;
         private Node<T>? next;
+
+        public Node(T value)
+        {
+            _value = value;
+            this.next = null;
+        }
+        public override string ToString() => _value.ToString();
+
+        public Node<T> Next
+        {
+            get { return next ?? this; }
+            private set { next = value; }
+        }
     }
 }
