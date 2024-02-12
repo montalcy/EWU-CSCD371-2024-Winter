4f1508fdc6fd22791a509fde74a3508ce9180c7b
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-02-11 20:38:01 -0800

    Fixing Next

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -1,22 +1,36 @@
-﻿namespace GenericsHomework
+﻿//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace GenericsHomework
 {
     public class Node<T>
     {
         private T _value { get; set; }
-        private Node<T>? Next {  get; set; }
-        private Node<T> Head { get { return this; } set { Head = this; } }
+        private Node<T> Next {  get; set; }
+        public int Size;
+        private Node<T> Head { get; set; }
 
         public Node(T value)
         {
             _value = value;
-            this.Next = null;
+            if (Size == 1) { Next = Head; }
+            else { Next = this; };
         }
-        public override string ToString() => _value.ToString();
 
-        public Node<T> Next
+        public T GetData()
         {
-            get { return Next ?? this; }
-            private set { Next = value; }
+            return _value;
+        }
+
+        override public string ToString()
+        {
+            Node<T> current = Head;
+            string list = "";
+            while (current.Next != Head)
+            {
+                list += current.GetData();
+            }
+
+            return "Your linked list: " + list;
         }
 
         public void Clear()
@@ -27,13 +41,61 @@
             }
         }
 
+
+        public void RemoveAll()
+        {
+            Node<T> curr = Head;
+            Node<T> prev;
+            while (curr.Next.Next != Head)
+            {
+                prev = curr;
+                curr.Next = curr.Next.Next;
+                curr = curr.Next;
+            }
+            curr.Next = curr.Next.Next;
+        }
+
+
+
+
         public void Append(T value)
         {
+            Node<T> curr = Head;
+            Node<T> addition = new Node<T>(value);
+
+            if (Exists(value))
+            {
+                throw new ArgumentException("This already exists in the list");
+            }
+
+            if (Head.Next == Head)
+            {
+                Head = addition;
+                Size = 1;
+            }
+            else
+            {
+                while (Next != Head)
+                {
+                    curr = curr.Next;
+                }
+                curr.Next = addition;
+                Size++;
+            }
 
         }
-        private bool doesExists(T? value) // will be using it when appending
-        {
 
+        public bool Exists(T data)
+        {
+            Node<T> curr = Head;
+            while (curr != Head)
+            {
+                if (curr._value.Equals(data))
+                {
+                    return true;
+                }
+                curr = curr.Next;
+            }
             return false;
         }
     }
