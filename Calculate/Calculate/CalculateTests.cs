4fe71a2c5bcbae80c20ae8ce95ccb5fed9f29fb2
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-02-19 12:02:20 -0800

    Simple Tests

--- Calculate/Calculate/CalculateTests.cs
+++ Calculate/Calculate/CalculateTests.cs
@@ -10,5 +10,34 @@ namespace Calculate;
 public class CalculateTests
 {
     [Fact]
-    public 
+    public void Add_TwoValues_Successful()
+    {
+        int solution= Calculator.Add(2, 2);
+        Assert.Equal(4, solution);
+    }
+
+    [Fact]
+    public void Subtract_TwoValues_Successful()
+    {
+        int solution = Calculator.Subtract(2, 2);
+        Assert.Equal(0, solution);
+    }
+
+    [Fact]
+    public void Divide_TwoValues_Successful()
+    {
+        int solution = Calculator.Divide(2, 2);
+        Assert.Equal(1, solution);
+    }
+
+    [Fact]
+    public void Multiply_TwoValues_Successful()
+    {
+        int solution = Calculator.Multiply(2, 2);
+        Assert.Equal(4, solution);
+    }
+
+
 }
+
+
