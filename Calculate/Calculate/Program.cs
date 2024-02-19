a53699880f4ee7708feee0c6ceadc0dffbd6c83d
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-02-19 11:18:27 -0800

    classes

---
+++ Calculate/Calculate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate;
+
+public class Program
+{
+    public void WriteLine { get; set; } = System.Console.WriteLine;
+    public void ReadLine { get; set; } = System.Console.ReadLine;
+
+    public Program() {}
+}
