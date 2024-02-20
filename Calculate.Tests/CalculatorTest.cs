d6ec403f11dd7b0d6508dbc4abcd0b9db46f875a
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-02-19 22:14:04 -0800

    Changes
    Moved the buildprops, was causing errors, need to implement it back in. Got tests running and passing

--- Calculate.Tests/CalculatorTest.cs
+++ Calculate.Tests/CalculatorTest.cs
@@ -13,32 +13,28 @@ public class CalculatorTest
     [Fact]
     public void Add_TwoValues_Successful()
     {
-        Calculator calc = new();
-        int solution = calc.Add(2, 2);
+        int solution = Calculator.Add(2, 2);
         Assert.Equal(4, solution);
     }
 
     [Fact]
     public void Subtract_TwoValues_Successful()
     {
-        Calculator calc = new();
-        int solution = calc.Subtract(2, 2);
+        int solution = Calculator.Subtract(2, 2);
         Assert.Equal(0, solution);
     }
 
     [Fact]
     public void Divide_TwoValues_Successful()
     {
-        Calculator calc = new();
-        int solution = calc.Divide(2, 2);
+        int solution = Calculator.Divide(2,2);
         Assert.Equal(1, solution);
     }
 
     [Fact]
     public void Multiply_TwoValues_Successful()
     {
-        Calculator calc = new();
-        int solution = calc.Multiply(2, 2);
+        int solution = Calculator.Multiply(2, 2);
         Assert.Equal(4, solution);
     }
 
--- Calculate/Calculator.cs
+++ Calculate/Calculator.cs
@@ -24,7 +24,6 @@ public class Calculator
     public bool TryCalculate(string expression, out int solution)
     {
         solution = 0;
-        solution = null;
         return false;
     }
 }
